b6677d298c0be1921fa5c4341b567709e128ee59
The Ace <udonsichisom02@gmail.com>
2023-08-03 07:07:44 +0100

    feat: created slider component

--- stackclique/src/components/Layout/Authentication.jsx
+++ stackclique/src/components/Layout/Authentication.jsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
+import Slider from "./Slider";
 export default function Authentication() {
     return (
         <div className="flex">
-            <section className="min-w-[55%] hidden lg:block">slider </section>
+            <Slider />
             <section className="w-[90%] mx-auto lg:min-w-[45%] my-[2rem] relative">
                 <h3 className="text-primary font-poppins font-[800]">
                     STACK<span className="text-green">CLIQUE</span>
---
+++ stackclique/src/components/Layout/Slider.jsx
@@ -0,0 +1,3 @@
+export default function Slider() {
+    return <section className="min-w-[55%] hidden lg:block">slider </section>;
+}
