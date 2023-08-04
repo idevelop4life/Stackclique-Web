e3eb110edf63cd2ee8ebe063ac812268d191b912
Sosaristic <sundayomena9@gmail.com>
2023-08-04 03:55:04 +0100

    added disabled border to button

--- stackclique/src/components/ui/Button.jsx
+++ stackclique/src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, onClick, disabled, type, bgWhite }) {
     return (
         <button
-            className={` disabled:bg-darkGrey hover:bg-primaryDark hover:text-white transition-all duration-200 ${
+            className={` disabled:bg-darkGrey disabled:border-darkGrey hover:bg-primaryDark hover:text-white transition-all duration-200 ${
                 bgWhite ? "bg-white text-primary" : "bg-primary text-white"
             } w-full py-[0.8125rem] rounded-[2rem] border border-primary`}
             onClick={onClick}
