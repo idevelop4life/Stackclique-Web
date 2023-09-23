5f5382e7a3f3a2bb9b025d4452a38e37d3ff5ff7
Sosaristic <sundayomena9@gmail.com>
2023-09-23 23:03:21 +0100

    fixed when a user engages with the input in slider

--- stackclique/src/components/Layout/applayout/Footer.jsx
+++ stackclique/src/components/Layout/applayout/Footer.jsx
@@ -20,7 +20,7 @@ const socialIcons = [
 export default function Footer() {
     const date = new Date().getFullYear();
     return (
-        <footer className=" flex flex-col md:flex-row gap-y-8 gap-x-4 font-manrope text-sm bg-black px-4 lg:px-[6rem] text-[#cbd5e1] py-4">
+        <footer className="min-h-fit flex relative flex-col md:flex-row gap-y-8 gap-x-4 font-manrope text-sm bg-black px-4 py-8 pb-[4rem]  lg:px-[6rem]  text-[#cbd5e1]">
             <div className="flex flex-col gap-4 flex-1">
                 <h2 className="text-white font-bold text-md">Legal</h2>
                 <Link to={"#"} className="hover:underline">
@@ -51,7 +51,7 @@ export default function Footer() {
                 <div className="h-[3rem] w-[90%] md:w-full  relative bg-white flex">
                     <input
                         type="text"
-                        placeholder="Join our newsletter, stay updated"
+                        placeholder="Your email address"
                         className=" px-4 text-xs md:text-sm  w-[60%] text-darkGrey outline-none"
                         // value={newsletterValue}
                         // onChange={(e) => setNewsLetterValue(e.target.value)}
@@ -79,7 +79,11 @@ export default function Footer() {
                     Help Center
                 </Link>
             </div>
-            {/* <div className="absolute">copyright {date}</div> */}
+            <div className="absolute flex bottom-0 w-[80%] justify-center text-xs left-[50%] -translate-x-1/2 ">
+                Copyright © {date} StackClique
+            </div>
+            <span className=" absolute left-0 bottom-0 h-[1.5rem] md:h-[2rem] w-1/4 md:w-1/3 bg-white rounded-tr-[3rem]" />
+            <span className=" absolute right-0 bottom-0 h-[1.5rem] md:h-[2rem] w-1/4 md:w-1/3 bg-white rounded-tl-[3rem]" />
         </footer>
     );
 }
