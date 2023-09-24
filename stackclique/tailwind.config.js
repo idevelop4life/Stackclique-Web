26206855f688308c922d2f77d2b614a93ee913d4
Sosaristic <sundayomena9@gmail.com>
2023-09-24 02:01:15 +0100

    added about our app section

--- stackclique/tailwind.config.js
+++ stackclique/tailwind.config.js
@@ -23,6 +23,9 @@ export default {
                 header: "rgb(110, 114, 122)",
                 backdrop: "rgba(245, 241, 243, 0.33)",
             },
+            boxShadow: {
+                aboutAppCardsShadow: "0px 1px 10px 0px rgba(0, 0, 0, 0.10)",
+            },
         },
     },
     plugins: [],
