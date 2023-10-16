41fdf1f4122232bed0b0bf488cf15bc5cff4e685
Moses <Moseschizaram8@gmail.com>
2023-10-16 14:29:52 +0100

    added the logic fo the ermail verification

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([
+    'verify' => true
+]);
+
+// The email verification Notice
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+// The email verification handler
+
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+ 
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+ 
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+
+// Resendint the verification email
+use Illuminate\Http\Request;
+ 
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
