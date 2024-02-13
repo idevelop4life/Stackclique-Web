1c6e419c51891703b4b71de641c0fff04f108806
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-02-13 07:41:57 +0100

    homepage hero section tweak #03

--- stackclique/src/components/ChatComponents/BubbleReply.jsx
+++ stackclique/src/components/ChatComponents/BubbleReply.jsx
@@ -2,7 +2,6 @@ import { shortText } from "../../utils/utils";
 import styles from "../../styles/css/app.module.css";
 
 function BubbleReply({ profile, text, userInfo }) {
-    console.log("User Info: ", userInfo, "\n", "Reply Info: ", profile);
     return (
         <div className={styles.chat_reply}>
             <h5>{userInfo?.id === profile?.id ? "You" : profile?.name}</h5>
