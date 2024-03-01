7c05461440f6cf833b7e541e2a7485789c9f22f3
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-03-01 06:51:23 +0100

    view course ui

--- stackclique/src/components/DetailsPage/index.js
+++ stackclique/src/components/DetailsPage/index.js
@@ -1,3 +1,8 @@
 export { default as Hero } from "./CourseHero";
 export { default as Outline } from "./CourseOutline";
 export { default as Dropdown } from "./CourseDropdown";
+export { default as Instructor } from "./Instructor";
+export { default as Enroll } from "./EnrollCourse";
+export { default as View } from "./ViewCourse";
+export { default as Playlist } from "./Playlist";
+export { default as Info } from "./CourseInfo";
